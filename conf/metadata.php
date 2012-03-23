34f629b4bab51b69c1edf1f967ea644c06171a3b
einhirn <marg@rz.tu-clausthal.de>
2012-03-23 18:17:37 +0100

    New files for option to override system-wide auth-module

---
+++ conf/metadata.php
@@ -0,0 +1,4 @@
+<?php
+
+$meta['auth']     = array('string');
+
---
+++ lang/en/settings.php
@@ -0,0 +1,3 @@
+<?php
+
+$lang['auth'] = "Name of the auth module to override the system-wide setting for searching users. Useful with \"chained\" or similar auth modules";
